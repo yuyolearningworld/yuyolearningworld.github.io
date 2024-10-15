f32ab265867798a294b581d70fe195ca7d43c93b
yuyolearningworld <urronj@gmail.com>
2024-10-15 13:30:15 +0100

    test

--- learning.js
+++ learning.js
@@ -1,14 +1,11 @@
 
 
 function learn(){
-	//var testhash = CryptoJS.MD5(event.srcElement.id).toString();
 	fetch('./lessons.json')
 	.then(response => response.json())
 	.then(response => {
-	
 		console.log(response)
-	
 	})
-
 }
+
 function quiz(){}
