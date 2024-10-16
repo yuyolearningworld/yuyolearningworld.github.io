58bc6ea56fed680cb61b32d06d5f3c220dc72d7b
yuyolearningworld <urronj@gmail.com>
2024-10-16 09:50:36 +0100

    test

--- learning.js
+++ learning.js
@@ -1,11 +1,14 @@
-var data
-fetch('./lessons.json')
-	.then(response => response.json())
-	.then(response => {
-		var data = response
-	})
+function getjson(){
+	fetch('./lessons.json')
+		.then(response => response.json())
+		.then(response => {
+			var data = response
+			return data
+		})
+}
 
 function learn(subject, lesson){
+	data = getjson()
 	console.log(data)
 }
 
