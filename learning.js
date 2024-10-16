b43e2b040a03cb19b9189fec3ebd03b9f24ff408
yuyolearningworld <urronj@gmail.com>
2024-10-16 09:21:54 +0100

    test

--- learning.js
+++ learning.js
@@ -1,10 +1,10 @@
-
+var data
 
 function learn(subject, lesson){
 	fetch('./lessons.json')
 	.then(response => response.json())
 	.then(response => {
-		globalThis.data = response
+		data = response
 	})
 
 	console.log(data)
