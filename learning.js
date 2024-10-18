7dcad4cbb6d74c067e0fc09a90a65f8923529bcc
yuyolearningworld <urronj@gmail.com>
2024-10-18 10:17:30 +0100

    test

--- learning.js
+++ learning.js
@@ -3,8 +3,14 @@ function learn(subject, lesson){
 	fetch('./lessons.json')
 		.then(response => response.json())
 		.then(response => {
-			var totalitems = Object.keys(response["__" + subject + lesson + "__"])
+
+			currentdata = "__" + subject + lesson + "__"
+
+			var totalitems = Object.keys(response[currentdata])
 			console.log(totalitems)
+
+			var title = response[currentdata][totalitems[0]][0]
+			console.log(title)
 		})
 	}
 
