9baeea94fdcbe4070885912585adefbe9227521e
yuyolearningworld <urronj@gmail.com>
2024-10-18 10:08:33 +0100

    test

--- learning.js
+++ learning.js
@@ -4,10 +4,7 @@ function learn(subject, lesson){
 		.then(response => response.json())
 		.then(response => {
 			var totalitems = Object.keys(response["__" + subject + lesson + "__"])
-			var data = []
-			for(i=0; i<totalitems.length; i++){
-				console.log(response[totalitems[i]])
-			}
+			console.log(totalitems)
 		})
 	}
 
