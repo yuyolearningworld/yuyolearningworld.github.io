81f695e39f31dc1240bc290a207f54629d8aabf2
yuyolearningworld <urronj@gmail.com>
2024-10-18 09:35:52 +0100

    test

--- learning.js
+++ learning.js
@@ -3,8 +3,7 @@ function learn(subject, lesson){
 	fetch('./lessons.json')
 		.then(response => response.json())
 		.then(response => {
-			var data = response
-			console.log(data["__" + subject + lesson + "__"][pages][page1])
+			console.log(response["__" + subject + lesson + "__"]["pages"]["page1"])
 		})
 	}
 
