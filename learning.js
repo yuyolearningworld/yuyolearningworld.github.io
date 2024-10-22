db268c9b7e8d77d036b2c602e1227c416c025ca1
yuyolearningworld <urronj@gmail.com>
2024-10-22 15:52:53 +0100

    test

--- learning.js
+++ learning.js
@@ -1,23 +1,21 @@
 
 function learn(subject, lesson){
-	document.getElementById("animation_frame").style.animationName = "pagechange_left"
-    document.getElementById("animation_frame").style.animationDuration = "2s"
-    document.getElementById("animation_frame").style.animationFillMode = "forwards"
-    setTimeout(function(){window.location.replace("lesson.html")},2000)  
+	window.location.replace("lesson.html")  
 	fetch('./lessons.json')
 		.then(response => response.json())
 		.then(response => {
-			currentdata = "__" + subject + lesson + "__"
-			var totalitems = Object.keys(response[currentdata])
-			var title = response[currentdata]["title"]
-			var numofpages = totalitems.length - 1
-			var pageContents = []
-			for (i=1; i<numofpages; i++){
-				pageContents.push(response[currentdata]["page" + i])
-			}
-
-			document.write("test if can write in other documents")
-		})
+	currentdata = "__" + subject + lesson + "__"
+	var totalitems = Object.keys(response[currentdata])
+	var title = response[currentdata]["title"]
+	var numofpages = totalitems.length - 1
+	var pageContents = []
+	for (i=1; i<numofpages; i++){
+		pageContents.push(response[currentdata]["page" + i])
 	}
+
+	document.write("test if can write in other documents")
+		
+	})
+}
 
 function quiz(){}
