6da5e1b408bdccf0e9d521ba612d9e926f7b1575
yuyolearningworld <urronj@gmail.com>
2024-10-22 16:20:55 +0100

    test

--- learning.js
+++ learning.js
@@ -11,7 +11,33 @@ function learn(subject, lesson){
 		pageContents.push(response[currentdata]["page" + i])
 	}
 	var lessonpage = window.open("lesson.html")
-	lessonpage.document.write("<title> test </title>")
+	
+	var html = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Yuyo Learning World</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    
+    <script src="ribbonbuttons.js"></script>
+    
+    <img id="animation_frame" src="assets\\animation_frame.png">
+
+    <div class="grid_ribbon">
+        <img id="logo" src="assets\\yuyo_logo.png">
+        <img onclick="subjectpress()" id="subjects" src="assets\\subjects.png">
+        <img  onclick="koikoipress()" id="koikoi" src="assets\\koikoi.png">
+        <img  onclick="settingspress()" id="settings" src="assets\\settings.png">
+    </div>
+    <img id="ribbon_underline" src="assets\\ribbon_underline.png">
+</body>
+</html>
+`
+	lessonpage.document.write(html)
 		
 	})
 }
