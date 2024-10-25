24e06ed54eaa6d09fbfaa43876b15b7c34c9b51b
yuyolearningworld <urronj@gmail.com>
2024-10-25 12:36:45 +0100

    test

--- learning.js
+++ learning.js
@@ -27,7 +27,7 @@ function learn(subject, lesson, page){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>` + title + `</title>
     <link rel="stylesheet" type="text/css" href="style.css">
-	<link rel="icon" href="assets\\tablogo.png">
+	<link rel="icon" type="image/x-icon" href="assets\\tablogo.png">
 </head>
 <body>
 
