2756a4c2df2f7f876a9ad55a269dba27450244b2
yuyolearningworld <urronj@gmail.com>
2024-10-29 12:09:05 +0000

    test

--- learning.js
+++ learning.js
@@ -1,33 +1,24 @@
-globalThis.currentsubject = null
-globalThis.currentlesson = null
-globalThis.currentpage = null
-globalThis.totalpages = null
-globalThis.lessonhtml = null
-globalThis.html = null
 
 function learn(subject, lesson, page){
-	globalThis.currentsubject = subject
-	globalThis.currentlesson = lesson
-	globalThis.currentpage = page
 	fetch('./lessons.json')
 		.then(response => response.json())
 		.then(response => {
 	currentdata = "__" + subject + lesson + "__"
 	var totalitems = Object.keys(response[currentdata])
 	var title = response[currentdata]["title"]
-	globalThis.totalpages = totalitems.length - 1
+	var totalpages = totalitems.length - 1
 	var pageContents = []
 	for (i=1; i<totalpages; i++){
 		pageContents.push(response[currentdata]["page" + i])
 	}
-	globalThis.lessonhtml = window.open("lesson.html")
-	globalThis.html = `
+	var lessonhtml = window.open("lesson.html")
+	var html = `
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>` + title + ` - Page `+ (page+1) +`</title>
+    <title>` + title + `</title>
     <link rel="stylesheet" type="text/css" href="style.css">
 	<link rel="icon" type="image/x-icon" href="assets\\tablogo.png">
 </head>
@@ -40,6 +31,12 @@ function learn(subject, lesson, page){
 
 	<!-- <img class="yuyo" src="yuyo_sprites\\yuyo0.png"> -->
 
+	<script>
+		var data = [` + pageContents + `]
+		console.log(data)
+
+	</script>
+
 	<img class="arrow" id="rightarrow" onclick="increasePage(` + (page+1) + `)" src="assets\\rightarrow.png">
 	<img class="arrow" id="leftarrow" onclick="decreasePage(` + (page-1) + `)" src="assets\\leftarrow.png">
 
@@ -50,12 +47,4 @@ function learn(subject, lesson, page){
 	lessonhtml.document.write(html)
 
 	})
-}
-
-function increasePage(page){
-	learn(currentsubject, currentlesson, page)
-}
-
-function decreasePage(page){
-		learn(currentsubject, currentlesson, page)
 }
