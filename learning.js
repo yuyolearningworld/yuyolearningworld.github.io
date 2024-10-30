196fb75dff02679f7d696e9e5d6a41d5c467bfd6
yuyolearningworld <urronj@gmail.com>
2024-10-30 16:12:18 +0000

    test

--- learning.js
+++ learning.js
@@ -1,17 +1,19 @@
 
 function learn(subject, lesson){
+	document.getElementById("animation_frame").style.animationName = "pagechange_left"
+    document.getElementById("animation_frame").style.animationDuration = "2s"
+    document.getElementById("animation_frame").style.animationFillMode = "forwards"
 	fetch('./lessons.json')
 		.then(response => response.json())
 		.then(response => {
 	currentdata = "__" + subject + lesson + "__"
 	var totalitems = Object.keys(response[currentdata])
 	var title = response[currentdata]["title"]
-	var totalpages = totalitems.length - 1
 	var pageContents = []
-	for (i=1; i<totalpages; i++){
-		pageContents.push(response[currentdata]["page" + i])
+	for (i=1; i<totalitems.length; i++){
+		pageContents.push('"' + response[currentdata]["page" + i] + '"')
 	}
-	var lessonhtml = window.open("lesson.html")
+	setTimeout(function(){var lessonhtml = window.open("lesson.html")
 	var html = `
 <!DOCTYPE html>
 <html lang="en">
@@ -27,25 +29,45 @@ function learn(subject, lesson){
 	<script src="learning.js"></script>
 
 	<p id="lessontitle">` + title + `</p>
-	<div id="lessontextbox"> </div>
 
-	<!-- <img class="yuyo" src="yuyo_sprites\\yuyo0.png"> -->
+	<div id="lessontextbox"> </div>
+	<img id="learningyuyo" src="yuyo_sprites\\yuyo0.png">
+	
 
 	<script>
+
+		
+
+		function yuyospeak(text) {
+			var i = 0
+			var text = text
+			function main(counter) {
+				var i = counter
+				if (i < text.length) {
+				document.getElementById("lessontextbox").innerText += text.charAt(i)
+				var i = i + 1
+				setTimeout(funtion(){main(i)}, 100)
+				}
+			}
+			main(i)
+		}
+			
+	
+
 		var data = [` + pageContents + `]
 		var current = 0
-		document.getElementById("lessontextbox").innerText = data[current]
+		setTimeout(yuyospeak(data[current]),50)
 
 		function increasePage(){
 			if (current + 1 > data.length-1) {return}
 			globalThis.current =  current + 1
-			document.getElementById("lessontextbox").innerText = data[current]
+			yuyospeak(data[current])
 		}
 
 		function decreasePage(){
 			if (current - 1 < 0) {return}
 			globalThis.current =  current - 1
-			document.getElementById("lessontextbox").innerText = data[current]
+			yuyospeak(data[current])
 		}
 
 	</script>
@@ -59,6 +81,9 @@ function learn(subject, lesson){
 `
 
 	lessonhtml.document.write(html)
-
+	setTimeout(function(){
+		document.getElementById("animation_frame").style.animationName = "pagechange_right"
+		document.getElementById("animation_frame").style.animationDuration = "2s"
+		document.getElementById("animation_frame").style.animationFillMode = "forwards"},500)},2000)
 	})
 }
