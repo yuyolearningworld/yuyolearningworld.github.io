c99d26a5264c4f360c25876f55580af1ce02931a
yuyolearningworld <urronj@gmail.com>
2024-11-01 01:09:53 +0000

    test

--- ribbonbuttons.js
+++ ribbonbuttons.js
@@ -6,10 +6,13 @@ function subjectpress(){
 }
 
 function koikoipress(){
-    document.getElementById("animation_frame").style.animationName = "pagechange_left"
-    document.getElementById("animation_frame").style.animationDuration = "2s"
-    document.getElementById("animation_frame").style.animationFillMode = "forward"
-    setTimeout(function(){window.location.replace("koikoi.html")},2000)
+    var w = 1000
+    var h = 700
+    var koikoi = window.open('koikoi.html', 'KoiKoi','resizable=0,width='+w+',height='+h+'')
+    koikoi.addEventListener("resize", () => {
+        koikoi.resizeTo(w, h);
+    })
+
 }
 
 function settingspress(){
