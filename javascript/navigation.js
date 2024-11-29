5b9d566a884b0c34f6d55bfac4c15273bfe54667
yuyolearningworld <urronj@gmail.com>
2024-11-29 15:11:46 +0000

    new update part 2

--- javascript/navigation.js
+++ javascript/navigation.js
@@ -4,7 +4,9 @@ function subjectpress(){
     document.getElementById("animation_frame").style.animationFillMode = "forwards"
     var click = new Audio('../misc/sounds/click.mp3');
     click.play();
-    setTimeout(function(){window.location.replace("subjects.html")},2000)  
+    setTimeout(function(){
+        window.location.replace("subjects.html")
+    },2000)  
 }
 
 function koikoipress(){
@@ -35,11 +37,57 @@ function musicpress(){
     click.play();
 }
 
-function introreplay(){
+/*////////////////////////////////////
+///////// Dark mode changer //////////
+////////////////////////////////////*/
+
+if (window.localStorage.length == 0){
+    window.localStorage.setItem("darkmode", "0")
+}
+function darklight(){
     document.getElementById("animation_frame").style.animationName = "pagechange_left"
     document.getElementById("animation_frame").style.animationDuration = "2s"
-    document.getElementById("animation_frame").style.animationFillMode = "forward"
-    setTimeout(function(){window.location.replace("../index.html")},2000)
-    var click = new Audio('../misc/sounds/click.mp3');
-    click.play();
+    document.getElementById("animation_frame").style.animationFillMode = "forwards"
+    setTimeout(function(){
+        if (window.localStorage.getItem("darkmode") == 1) {
+            window.localStorage.setItem("darkmode", "0")
+        } else {
+            window.localStorage.setItem("darkmode", "1")
+        }
+        
+        if (document.getElementById("darkmode").style.opacity == 1){
+            document.getElementById("darklight").setAttribute("src", "../misc/assets/lightmode.png")
+        } else {
+            document.getElementById("darklight").setAttribute("src", "../misc/assets/darkmode.png")
+        }
+
+    },1500)
+    setTimeout(function(){
+        document.getElementById("animation_frame").style.animationName = "pagechange_right"
+        document.getElementById("animation_frame").style.animationDuration = "2s"
+        document.getElementById("animation_frame").style.animationFillMode = "forwards"
+    },2400)
 }
+
+setInterval(function(){
+    if (window.localStorage.getItem("darkmode") == 0){
+        document.getElementById("darkmode").style.opacity = 0
+        document.body.style.cursor = "url('../misc/assets/cursor.png'), default";
+        try{ document.getElementById("darklight").setAttribute("src", "../misc/assets/darkmode.png")}catch(err){}
+    } 
+    else if (window.localStorage.getItem("darkmode") == 1) {
+        document.getElementById("darkmode").style.opacity = 1
+        document.body.style.cursor = "url('../misc/assets/cursor-darkmode.png'), default";
+        try{ document.getElementById("darklight").setAttribute("src", "../misc/assets/lightmode.png")}catch(err){}
+    }
+},100)
+
+
+
+
+
+
+
+
+
+
